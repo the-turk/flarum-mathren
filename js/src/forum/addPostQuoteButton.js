692cc739c80d350993155f615bceb7a74be37b08
魏子轩 <weizixuan110@foxmail.com>
2021-04-05 16:16:51 +0800

    Create addPostQuoteButton.js

--- js/src/forum/addPostQuoteButton.js
+++ js/src/forum/addPostQuoteButton.js
@@ -28,33 +28,29 @@
 
 import { extend } from 'flarum/extend';
 import CommentPost from 'flarum/components/CommentPost';
-import PostQuoteButton from './components/PostQuoteButton';
+
+import PostQuoteButton from './fragments/PostQuoteButton';
 import selectedText from './utils/selectedText';
-import copyDelimiters from './utils/copyDelimiters';
 
 export default function addPostQuoteButton() {
-  extend(CommentPost.prototype, 'config', function (original, isInitialized) {
-    if (!app.forum.attribute('mathRenAddQuoteButton') || !app.forum.attribute('mathRenEnableCopyTeX')) return;
+  extend(CommentPost.prototype, 'oncreate', function() {
+    const post = this.attrs.post;
 
-    const post = this.props.post;
-    const delimiters = {
-      inline: app.forum.attribute('mathRenMainInlineDelimiter'),
-      block: app.forum.attribute('mathRenMainBlockDelimiter'),
-    };
-
-    if (isInitialized || post.isHidden() || (app.session.user && !post.discussion().canReply())) return;
+    if (post.isHidden() || (app.session.user && !post.discussion().canReply())) return;
 
     const $postBody = this.$('.Post-body');
 
     // Wrap the quote button in a wrapper element so that we can render
     // button into it.
-    const $container = $('<div class="Post-quoteButtonContainer-mathRen"></div>');
+    const $container = $('<div class="Post-quoteButtonContainer"></div>');
 
-    const handler = function (e) {
+    const button = new PostQuoteButton(post);
+
+    const handler = function(e) {
       setTimeout(() => {
-        const content = selectedText($postBody, copyDelimiters(delimiters));
+        const content = selectedText($postBody);
         if (content) {
-          const button = new PostQuoteButton({ post, content });
+          button.content = content;
           m.render($container[0], button.render());
 
           const rects = window.getSelection().getRangeAt(0).getClientRects();
